440ac64cea03369506be47c6612bdddfb396f7fc
Amey Jadhav <141218313+jadhavamey@users.noreply.github.com>
2024-10-14 14:54:35 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,17 +7,13 @@ module "vpc" {
 
   azs = local.azs
 
-  # Define subnets
   public_subnets  = local.public_subnets
   private_subnets = local.private_subnets
   intra_subnets   = local.intra_subnets
 
-  # Enable NAT gateway for outbound internet access from private subnets
   enable_nat_gateway = true
 
-  # Optionally, configure tags for the VPC and subnets
   tags = {
-    Name        = local.name
-    Environment = "dev"  # Adjust as necessary
+    Name = local.name
   }
 }
