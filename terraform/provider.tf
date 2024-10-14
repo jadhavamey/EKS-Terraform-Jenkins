c9b9a3cfb73941c661b2aeb085abb5b06b718bf4
Amey Jadhav <141218313+jadhavamey@users.noreply.github.com>
2024-10-14 15:42:02 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ locals {
   public_subnets = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets  = ["10.123.5.0/24", "10.123.6.0/24"]
-
+  
   # Keeping only necessary tags
   tags = {
     Name = local.name
