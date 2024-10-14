0cfc210540df2ea1f0824457729c81e6133da34d
Amey Jadhav <141218313+jadhavamey@users.noreply.github.com>
2024-10-14 12:56:58 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,19 @@
 locals {
-  region = "us-east-1"
-  name   = "eks"
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  region        = "us-east-1"
+  name          = "eks"
+  vpc_cidr      = "10.123.0.0/16"
+  azs           = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  intra_subnets  = ["10.123.5.0/24", "10.123.6.0/24"]
+  
+  # You can choose to add or remove tags as needed
   tags = {
-    Example = local.name
+    Name = local.name
+    Environment = "dev"  # Example of additional tag
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = local.region
 }
